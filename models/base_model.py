fc66ac3c2f191c88a4fba1a5bcf5fd592564cc4d
Stephen Oba <obastepheno@gmail.com>
2022-10-25 13:57:04 +0100

    Add base_model module with tasks to be completed

---
+++ models/base_model.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# base_model.py
+"""Module contains BaseModel class that povides a template for
+subclasses
+"""
+
+
+class BaseModel:
+    """Defines all common attributes/methods for subclasses
+    """
+    # Todo: implement __init__ method with attributes
+    #   id, created_at, and updated_at
+
+    # Todo: Add __str__ magic method
+
+    # Todo: Add public instance method `save(self)`
+
+    # Todo: Add public instance method `to_dict(self)`
+
+    pass
