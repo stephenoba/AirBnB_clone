dd267261e1ce48fc2f4df06f1e8d9a8dc16b551c
Stephe Oba <obastepheno@gmail.com>
2022-10-29 17:41:28 +0100

    refractor do_create and add clean_args method

--- console.py
+++ console.py
@@ -8,6 +8,9 @@ from models.base_model import BaseModel
 class HBNBCommand(cmd.Cmd):
     """Defines the command intepreter"""
     prompt = "(hbnb) "
+    __classes = {
+            "BaseModel": BaseModel,
+            }
 
     def emptyline(self):
         """Does nothing when it receives an empty line"""
@@ -26,11 +29,22 @@ class HBNBCommand(cmd.Cmd):
         """Creates a new instance of"""
         if not arg:
             print("** class name missing **")
-        elif arg != "BaseModel":
-            print("** class doesn't exist **")
-        else:
-            _obj = BaseModel()
+
+        args = self.clean_args(arg)
+        cls_name = args[0]
+        if cls_name in self.__classes:
+            _obj = self.__classes[cls_name]()
             print(_obj.id)
+        else:
+            print("** class doesn't exist **")
+
+    @staticmethod
+    def clean_args(args: str):
+        """
+        clean arguments
+        """
+        args = args.strip().split(' ')
+        return args
 
 
 if __name__ == '__main__':
