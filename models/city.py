fdce2158bcbbf6921a09ceb39bed889dc99e092a
Stephen Oba <obastepheno@gmail.com>
2022-10-30 20:51:16 +0100

    Add City module and tests

---
+++ models/city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module holds City class definition
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+    City model
+    """
+    state_id = ""
+    name = ""
