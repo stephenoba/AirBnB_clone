ed6ed91f19e3127cf42d94792e1c8d39ca134857
Cynthia Uche <cynthianuchepro@gmail.com>
2022-10-30 23:51:20 -0700

    Update unittest

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -161,7 +161,7 @@ class TestFileStorage_methods(unittest.TestCase):
         self.assertIn("Review." + rv.id, objs)
 
     def test_reload_no_file(self):
-        self.assertRaises(FileNotFoundError, models.storage.reload())
+        self.assertRaises(TypeError, models.storage.reload())
 
     def test_reload_with_arg(self):
         with self.assertRaises(TypeError):
